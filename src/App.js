35f3e35e99d7d15ba847207f93aa766b7878f656
InvictusSolar <nicky.mann409@gmail.com>
2025-05-14 20:05:49 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'Arial' }}>
+      <h1>Invictus Job Manager</h1>
+      <p>React and Firebase app is connected successfully.</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCG9IcEuW2mhQ4fcqHS92NpGwaA5fGFwKM",
+  authDomain: "invictus-job-manager.firebaseapp.com",
+  projectId: "invictus-job-manager",
+  storageBucket: "invictus-job-manager.firebasestorage.app",
+  messagingSenderId: "146884492392",
+  appId: "1:146884492392:web:f28074d8221970599d4fdd",
+  measurementId: "G-P6VQSWLDY5"
+};
